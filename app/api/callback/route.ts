fea817553060506425d6ce7dcb46c978bf621504
Maarten <lopesmaa@bu.edu>
2025-12-03 21:13:18 -0500

    made adjustments to fix the vercel deployment and add tailwind css

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -4,10 +4,8 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const code = searchParams.get("code");
 
-  //If no code is returned from GitHub, show error
   if (!code) return NextResponse.json({ error: "No code provided" });
 
-  //Exchange authorization code for access token
   const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { "Content-Type": "application/json", Accept: "application/json" },
@@ -22,7 +20,6 @@ export async function GET(req: Request) {
   const tokenData = await tokenRes.json();
   const accessToken = tokenData.access_token;
 
-  // Fetch user info from GitHub API
   const userRes = await fetch("https://api.github.com/user", {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
@@ -30,12 +27,11 @@ export async function GET(req: Request) {
 
   console.log("Token data:", tokenData);
   console.log("User data:", user);
-  
-  //Redirect to profile page with user data in query params
+
   return NextResponse.redirect(
     new URL(
       `/profile?login=${user.login}&avatar=${user.avatar_url}&url=${user.html_url}&repos=${user.public_repos}&followers=${user.followers}`,
-      req.url
+      process.env.NEXT_PUBLIC_SITE_URL
     )
   );
 }
